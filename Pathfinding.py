281cab71a6babf107d896dc385601c75964b8a92
Zitrone Koma30 <leon.andrassik@gmail.com>
2021-10-11 20:44:41 +0200

    added grid

--- Pathfinding.py
+++ Pathfinding.py
@@ -12,19 +12,40 @@ screen = pg.display.set_mode(SCREEN_DIMENSIONS);
 pg.display.set_caption(CAPTION)
 
 #classes
+class BackgroundRenderer():
+    def __init__(self, surface, width, height, grid_size):
+        self.surface = surface
+        self.width = width
+        self.height = height
+        self.grid_size = grid_size
+    
+    def draw_background(self):
+        for y in range(self.height):
+            for x in range(self.width):
+                pg.draw.rect(self.surface, (255, 255, 255), pg.Rect(x * self.grid_size, y * self.grid_size, self.grid_size - 2, self.grid_size - 2))
+
 class Pathfinder:
-    def __init__(self, running, screen):
+    def __init__(self, running, screen, grid_size):
         self.running = running
         self.screen = screen
+        self.clock = pg.time.Clock()
+        self.grid_size = grid_size
+
+        #Instances
+        self.bg_renderer = BackgroundRenderer(self.screen, 500, 500, self.grid_size)
     
     def run_main_loop(self):
         '''runs the main loop, handles rendering as well as the logic'''
+        delta_time = self.clock.tick(120)
         while self.running:
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     self.running = False
 
             screen.fill((0, 0, 0))
+            
+            #rendering
+            self.bg_renderer.draw_background()
 
             #All the logic
 
@@ -32,7 +53,7 @@ class Pathfinder:
         
         pg.quit()
 
-pathfinder = Pathfinder(True, screen)
+pathfinder = Pathfinder(True, screen, 16)
 pathfinder.run_main_loop()
 
 
