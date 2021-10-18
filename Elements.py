9ab5b3a7b3b65b9d799e738f2b19745c61d9abc8
Zitrone Koma30 <leon.andrassik@gmail.com>
2021-10-18 17:05:41 +0200

    Implemented working Pathfinding

--- Elements.py
+++ Elements.py
@@ -3,29 +3,64 @@ import numpy as np
 
 
 class Cursor:
-    def __init__(self, position: list, size: list, color: tuple, grid_size: int):
+    def __init__(self, position: list, color: tuple, grid_size: int):
         self.position = position
-        self.size = size
         self.color = color
         self.grid_size = grid_size
 
-        self.rect = pg.Rect(np.floor(position[0]/self.grid_size)*self.grid_size, np.floor(position[1]/self.grid_size) * self.grid_size, size[0], size[1])
+        self.path = []
+        self.rect = pg.Rect(self.position[0]*self.grid_size, self.position[1]*self.grid_size, self.grid_size, self.grid_size)
+
+    def calculate_distance(self, x1, y1, x2, y2):
+        dist = np.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+        return dist
+
+    def update(self, obstacles: list, goal) -> list:
+        '''runs the pathfinding logic'''
+        options = []
+
+        #vertical
+        options.append(pg.Rect(self.position[0]*self.grid_size, (self.position[1]+1)*self.grid_size, self.grid_size, self.grid_size))
+        options.append(pg.Rect(self.position[0]*self.grid_size, (self.position[1]-1)*self.grid_size, self.grid_size, self.grid_size))
+
+        #horizontal
+        options.append(pg.Rect((self.position[0]+1)*self.grid_size, self.position[1]*self.grid_size, self.grid_size, self.grid_size))
+        options.append(pg.Rect((self.position[0]-1)*self.grid_size, self.position[1]*self.grid_size, self.grid_size, self.grid_size))
+
+        #as is
+        options.append(pg.Rect(self.position[0]*self.grid_size, self.position[1]*self.grid_size, self.grid_size, self.grid_size))
+
+        for option in options:
+            #if self.calculate_distance(option.left, option.top, goal.rect.left, goal.rect.top) > self.calculate_distance(self.rect.left, self.rect.top, goal.rect.left, goal.rect.top):
+            #    options.remove(option)
+            for option1 in options:
+                if self.calculate_distance(option1.left, option1.top, goal.rect.left, goal.rect.top) > self.calculate_distance(option.left, option.top, goal.rect.left, goal.rect.top):
+                    options.remove(option1)
+
+        
+        self.position = [options[0].left/self.grid_size, options[0].top/self.grid_size]
+        self.rect = pg.Rect(self.position[0]*self.grid_size, self.position[1]*self.grid_size, self.grid_size, self.grid_size)
+
+        self.path.append(self.rect)
+        return self.path
+        #print(len(options))
     
-    def update(self, surface):
-        self.rect = pg.Rect(np.floor(self.position[0]/self.grid_size)*self.grid_size, np.floor(self.position[1]/self.grid_size) * self.grid_size, self.size[0], self.size[1])
-        pg.draw(surface, self.color, self.rect)
+    
+    def draw(self, surface):
+        '''draws the cursor'''
+        pg.draw.rect(surface, self.color, self.rect)
 
 
 class Goal:
-    def __init__(self, position: list, size: list, color: tuple, grid_size: int):
+    def __init__(self, position: list, color: tuple, grid_size: int):
         self.position = position
-        self.size = size
         self.color = color
         self.grid_size = grid_size
         
-        self.rect = pg.Rect(np.floor(position[0]/self.grid_size)*self.grid_size, np.floor(position[1]/self.grid_size)*self.grid_size, size[0], size[1])
-    
+        self.rect = pg.Rect(self.position[0]*self.grid_size, self.position[1]*self.grid_size, self.grid_size, self.grid_size)
+        
     def draw(self, surface):
+        '''draws the goal'''
         pg.draw.rect(surface, self.color, self.rect)
 
 
@@ -48,7 +83,7 @@ class Obstacle:
         self.grid_size = grid_size
         self.color = (0, 0, 0)
         self.rect = pg.Rect(np.floor(position[0]/self.grid_size)*self.grid_size, np.floor(position[1]/self.grid_size)*self.grid_size, self.grid_size, self.grid_size)
-    
-    def draw_self(self):
+
+    def draw(self):
         '''draws self.rect at self.position in self.color'''
         pg.draw.rect(self.surface, self.color, self.rect)
--- Pathfinding.py
+++ Pathfinding.py
@@ -1,7 +1,7 @@
 import pygame as pg
 import numpy as np
 from Elements import BackgroundRenderer, Goal, Cursor, Obstacle
-
+import time
 
 #globals
 SCREEN_DIMENSIONS= [500, 500]
@@ -26,10 +26,11 @@ class Pathfinder:
         self.grid_size = grid_size
 
         self.obstacles = []
+        self.started = False
 
         #Instances
-        self.goal = Goal([400, 380], [self.grid_size, self.grid_size], (30, 30, 255), self.grid_size)
-        self.cursor = Cursor([20, 20], [self.grid_size, self.grid_size], (255, 255, 0), self.grid_size)
+        self.goal = Goal([25, 25], (0, 255, 0), self.grid_size)
+        self.cursor = Cursor([2, 2], (255, 0, 255), self.grid_size)
         self.bg_renderer = BackgroundRenderer(self.screen, int(np.floor(screen_dimensions[0]/self.grid_size)), int(np.floor(screen_dimensions[1]/self.grid_size)), self.grid_size)
     
     def calculate_distance(start: list, end: list):
@@ -56,35 +57,37 @@ class Pathfinder:
                 current_obstacle = Obstacle(self.screen, (mouse_x, mouse_y), self.grid_size)
                 self.obstacles.append(current_obstacle)
             
+            if pg.mouse.get_pressed()[1]:
+                self.started = True
+            
             if pg.mouse.get_pressed()[2]:
                 for obstacle in self.obstacles:
                     if obstacle.rect.collidepoint(mouse_x, mouse_y):
                         self.obstacles.remove(obstacle)
                 
-            for obstacle in self.obstacles:
-                obstacle.draw_self()
-                for obstacle_1 in self.obstacles:
-                    if obstacle.rect.colliderect(obstacle_1.rect) and obstacle_1 != obstacle:
-                         self.obstacles.remove(obstacle_1)
-                
-
-            print(len(self.obstacles))
-                
-
-
-            #draw goal
+            
+            #update logic
+            if self.started:
+                path = self.cursor.update(self.obstacles, self.goal)
+                #draw path
+                for rect in path:
+                    pg.draw.rect(self.screen, (30, 30, 30), rect)
+            else:
+                self.cursor.draw(screen)
+            
+            #draw goal and obstacles
             self.goal.draw(self.screen)
 
-            #set cursor position
-            
-
-
-
+            for obstacle in self.obstacles:
+                obstacle.draw()
+                for obstacle_1 in self.obstacles:
+                    if obstacle.rect.colliderect(obstacle_1.rect) and obstacle_1 != obstacle:
+                        self.obstacles.remove(obstacle_1)
 
 
 
             pg.display.update()
-        
+            #time.sleep(0.5)
         pg.quit()
 
 pathfinder = Pathfinder(True, screen, GRID_SIZE, SCREEN_DIMENSIONS)
